c638787f62f01e0a7ad632e862d67d4927822891
Dzianis Bohdan <bogdan.denv@gmail.com>
2024-09-15 19:00:24 +0300

    feat: TonConnectProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import WebApp from "@twa-dev/sdk";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import App from "./App.tsx";
 import "./index.css";
 
 WebApp.ready();
-
+const manifestUrl =
+  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
   </StrictMode>
 );
