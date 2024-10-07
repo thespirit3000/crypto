a8975f7408d6a82bda05fa1d2a7ba14d7d63c85e
Dzianis Bohdan <bogdan.denv@gmail.com>
2024-10-07 19:58:23 +0300

    feat: firebase hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
-/* export default defineConfig({
-  plugins: [react()],
-  //base: "/",
-  base: "/crypto/",
-});
- */
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
-    },
-  },
-  base: process.env.NODE_ENV === "production" ? "/crypto/" : "",
+  //base: "/crypto/",
 });
