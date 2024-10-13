a9749b0cd55ef8e50647beedcd5e8c62ed1d97ec
Dzianis Bogdan <bogdan.denv@gmail.com>
2024-10-13 20:56:44 +0300

    refactor: button style

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,5 +1,6 @@
 const taskItemClass =
   "flex justify-between items-center space-x-1 text-gray-dark font-semibold border rounded px-6 my-1";
+
 const TaskItem = (props) => {
   const task = props.task;
   return (
@@ -8,7 +9,7 @@ const TaskItem = (props) => {
         <h2>{task.title}</h2>
         <div>Reward:{task.reward} $</div>
       </div>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-yellow transitions-colors hover:text-white border border-blue-500 hover:border-transparent rounded h-8 w-16 ">
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-yellow transitions-colors hover:text-white border border-blue-500 hover:border-transparent rounded h-8 w-16">
         Start
       </button>
       {/* <Link to={item.url}>{item.title}</Link> */}
